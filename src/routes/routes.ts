78e909258277b6bb55b1ed150a426481ae45aaa3
Andrey Masiero <andreymasiero@gmail.com>
2023-08-17 13:46:46 +0100

    feat: create stats view and route

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,7 +1,19 @@
 import NotesView from '@/views/NotesView.vue';
+import StatsView from '@/views/StatsView.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 export const router = createRouter({
   history: createWebHistory(),
-  routes: [{ path: '/', name: 'notes', component: NotesView }],
+  routes: [
+    {
+      path: '/',
+      name: 'notes',
+      component: NotesView,
+    },
+    {
+      path: '/stats',
+      name: 'stats',
+      component: StatsView,
+    },
+  ],
 });
---
+++ src/views/StatsView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="stats">
+    <h1>Stats</h1>
+  </div>
+</template>
