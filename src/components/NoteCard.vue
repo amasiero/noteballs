0d02b446c15611fabb456389a8321f0aec16b7bf
amasiero <amasiero@users.noreply.github.com>
2023-08-17 15:40:01 +0000

    Apply prettier code styling

--- src/components/NoteCard.vue
+++ src/components/NoteCard.vue
@@ -1,13 +1,14 @@
 <template>
-    <div class="card mb-4">
-  <div class="card-content">
-    <div class="content">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro voluptatibus aut omnis enim. Harum impedit natus, iste odio facilis ut non officia, distinctio exercitationem itaque sint nobis, quam eligendi assumenda.
+  <div class="card mb-4">
+    <div class="card-content">
+      <div class="content">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro voluptatibus aut omnis enim. Harum impedit natus,
+        iste odio facilis ut non officia, distinctio exercitationem itaque sint nobis, quam eligendi assumenda.
+      </div>
     </div>
+    <footer class="card-footer">
+      <a href="#" class="card-footer-item">Edit</a>
+      <a href="#" class="card-footer-item">Delete</a>
+    </footer>
   </div>
-  <footer class="card-footer">
-    <a href="#" class="card-footer-item">Edit</a>
-    <a href="#" class="card-footer-item">Delete</a>
-  </footer>
-</div>
 </template>
--- src/views/NotesView.vue
+++ src/views/NotesView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import NoteCard from "@/components/NoteCard.vue";
-import NoteForm from "@/components/NoteForm.vue";
+  import NoteCard from '@/components/NoteCard.vue';
+  import NoteForm from '@/components/NoteForm.vue';
 </script>
 
 <template>
-    <NoteForm />
-    <NoteCard v-for="i in 3" :key="i"/>
+  <NoteForm />
+  <NoteCard v-for="i in 3" :key="i" />
 </template>
