76875af14f8294a8ce1a91abaa6abe51cf339d61
Andrey Masiero <andreymasiero@gmail.com>
2023-08-17 15:42:06 +0100

    feat: add navbar and some design

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
+  import NavBar from '@/components/NavBar.vue';
+  import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  <RouterLink to="/">Notes</RouterLink> |
-  <RouterLink to="/stats">Stats</RouterLink>
+  <NavBar />
+  <div class="container is-max-desktop px-2 py-4">
   <RouterView />
+</div>
 </template>
 
 <style>
-
-@import 'bulma/css/bulma.min.css';
+  @import 'bulma/css/bulma.min.css';
 </style>
