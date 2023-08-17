84d9568baf9cb248111b09229ea67d62719fa07b
Andrey Masiero <andreymasiero@gmail.com>
2023-08-17 18:03:48 +0100

    feat: add notes dynamically

--- src/components/NoteCard.vue
+++ src/components/NoteCard.vue
@@ -1,9 +1,25 @@
+<script setup lang="ts">
+  import {Note} from '@/types/note';
+import { computed } from 'vue';
+
+  interface NoteCardProps {
+    note: Note;
+  }
+
+  const props = defineProps<NoteCardProps>();
+
+  const charactersCount = computed(() => {
+    const number = props.note.content.length;
+    return `${number} character${number !== 1 ? 's' : ''}`;
+  });
+</script>
+
 <template>
   <div class="card mb-4">
     <div class="card-content">
-      <div class="content">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro voluptatibus aut omnis enim. Harum impedit natus,
-        iste odio facilis ut non officia, distinctio exercitationem itaque sint nobis, quam eligendi assumenda.
+      <div class="content is-flex is-flex-direction-column">
+        <p>{{ note.content }}</p>
+        <p class="tag is-normal is-align-self-flex-end">{{ charactersCount }}</p>
       </div>
     </div>
     <footer class="card-footer">
--- src/components/NoteForm.vue
+++ src/components/NoteForm.vue
@@ -1,14 +1,31 @@
+<script setup lang="ts">
+interface NoteFormProps {
+  modelValue: string;
+}
+
+withDefaults(defineProps<NoteFormProps>(), {
+  modelValue: '',
+});
+
+const emit = defineEmits(['update:modelValue', 'save']);
+
+const onInput = (event: Event) => {
+  const target = event.target as HTMLTextAreaElement;
+  emit('update:modelValue', target.value);
+};
+</script>
+
 <template>
   <div class="card has-background-success-dark p-4 mb-5">
     <div class="field">
       <div class="control">
-        <textarea class="textarea" placeholder="Add a new note"></textarea>
+        <textarea class="textarea" placeholder="Add a new note" :value="modelValue" @input="onInput"></textarea>
       </div>
     </div>
 
     <div class="field is-grouped is-grouped-right">
       <div class="control">
-        <button class="button is-success">Add new note</button>
+        <button class="button is-success" :disabled="!modelValue" @click.prevent="$emit('save')">Add new note</button>
       </div>
     </div>
   </div>
