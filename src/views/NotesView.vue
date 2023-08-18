756d43d28dfd7e6780e846b17e5e28fc33f99b72
Andrey Masiero <andreymasiero@gmail.com>
2023-08-18 07:39:06 +0100

    chore: ts-ignore once it not find the types on node_modules folder

--- src/views/NotesView.vue
+++ src/views/NotesView.vue
@@ -2,6 +2,7 @@
   import NoteCard from '@/components/NoteCard.vue';
   import NoteForm from '@/components/NoteForm.vue';
   import { useNotesStore } from '@/stores';
+  // @ts-ignore
   import { v4 as uuidv4 } from 'uuid';
   import { ref } from 'vue';
 
