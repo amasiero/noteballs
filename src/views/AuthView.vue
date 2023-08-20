2e665e063b055473213a90f2eefb67f33b6762b7
amasiero <amasiero@users.noreply.github.com>
2023-08-20 20:19:01 +0000

    Apply prettier code styling

--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -1,27 +1,25 @@
 <script setup lang="ts">
-  import {ref, computed} from 'vue';
+  import { computed, ref } from 'vue';
 
   const register = ref(false);
-  const formTitle = computed(() => register.value ? 'Register' : 'Sign in');
+  const formTitle = computed(() => (register.value ? 'Register' : 'Sign in'));
 </script>
 
 <template>
   <div class="tabs is-centered">
-  <ul>
-    <li :class="{'is-active': !register}"><a @click.prevent="register = false">Sign in</a></li>
-    <li :class="{'is-active': register}"><a @click.prevent="register = true">Register</a></li>
-  </ul>
-</div>
-<div class="card auth-form">
-  <div class="card-content">
-    <div class="title has-text-centered">
-      {{ formTitle }}
-    </div>
-    <div class="content">
-
+    <ul>
+      <li :class="{ 'is-active': !register }"><a @click.prevent="register = false">Sign in</a></li>
+      <li :class="{ 'is-active': register }"><a @click.prevent="register = true">Register</a></li>
+    </ul>
+  </div>
+  <div class="card auth-form">
+    <div class="card-content">
+      <div class="title has-text-centered">
+        {{ formTitle }}
+      </div>
+      <div class="content"></div>
     </div>
   </div>
-</div>
 </template>
 
 <style scoped>
