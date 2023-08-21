5cbaf2d726c6c3a10df6f520becc1e370542a430
Andrey Masiero <andreymasiero@gmail.com>
2023-08-21 15:47:39 +0100

    feat: add navigation guard to routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,31 +1,47 @@
+import { useAuthStore } from '@/stores';
 import AuthView from '@/views/AuthView.vue';
 import EditNoteView from '@/views/EditNoteView.vue';
 import NotesView from '@/views/NotesView.vue';
 import StatsView from '@/views/StatsView.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
-export const router = createRouter({
+const routes = [
+  {
+    path: '/',
+    name: 'notes',
+    component: NotesView,
+  },
+  {
+    path: '/edit/:id',
+    name: 'edit-note',
+    component: EditNoteView,
+  },
+  {
+    path: '/stats',
+    name: 'stats',
+    component: StatsView,
+  },
+  {
+    path: '/auth',
+    name: 'auth',
+    component: AuthView,
+  },
+];
+
+const router = createRouter({
   history: createWebHistory(),
-  routes: [
-    {
-      path: '/',
-      name: 'notes',
-      component: NotesView,
-    },
-    {
-      path: '/edit/:id',
-      name: 'edit-note',
-      component: EditNoteView,
-    },
-    {
-      path: '/stats',
-      name: 'stats',
-      component: StatsView,
-    },
-    {
-      path: '/auth',
-      name: 'auth',
-      component: AuthView,
-    },
-  ],
+  routes,
 });
+
+router.beforeEach(async (to, from) => {
+  const authStore = useAuthStore();
+  if (!authStore.user && to.name !== 'auth') {
+    return { name: 'auth' };
+  }
+
+  if (authStore.user && to.name === 'auth') {
+    return false;
+  }
+});
+
+export { router };
